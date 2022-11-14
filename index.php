2baa3e1843d856aaf715ed5dfb826a7678eff0e7
emma2625 <emmanuelodobo10@gmail.com>
2022-11-14 13:30:21 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daily Gossip</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Logo -->
+    <link rel="shortcut icon" href="assets/img/logo.png">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/lib/fontawesome/css/all.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="assets/lib/owl-carousel/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/lib/owl-carousel/css/owl.theme.default.min.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <section>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="assets/img/logo.png" height="40" alt="">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fas fa-bars"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <a class="nav-link"  href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"  href="#">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"  href="#">Sign Up</a>
+                  </li>
+                  <li class="nav-item">
+                    <form  role="search">
+                        <div class="input-group">
+                          <input class="form-control form-control-sm" type="search" placeholder="Search" aria-label="Search">
+                          <button class="btn btn-sm btn-outline-light" type="submit">
+                              <i class="fa fa-search"></i>
+                          </button>
+                        </div>
+                      </form>
+                  </li>
+                </ul>
+                
+              </div>
+            </div>
+          </nav>
+    </section>
+
+    <!-- Hero -->
+    <section id="hero" class="mt-5">
+        <div class="container">
+            <div class="row">
+                <a href="#" class="col-md-8 mb-3 ">
+                    <div class="post-wrap">
+                        <img src="assets/img/posts/news-1.jpg" class="img-fluid main-post h-100" alt="Trending">
+                        <div class="overlay"></div>
+                        <div class="caption">
+                            <p class="fs-2 fw-bold title">This is the Latest News</p>
+                            <p class="fs-5">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quibusdam blanditiis tempore at, dignissimos dolorem harum minus assumenda obcaecati repellat!...
+                            </p>
+                            <p class="fs-6">Date: 12th March 2022</p>
+                        </div>
+                    </div>
+                </a>
+                <div class="col-md-4 mb-2">
+                    <div class="row">
+                        <a href="#" class="col-12 mb-2">
+                            <div class="post-wrap">
+                                <img src="assets/img/posts/news-2.jpg" alt="" class="img-fluid side-post">
+                                 <div class="overlay"></div>
+                                 <div class="caption">
+                                    <p class="fs-4 fw-bold title">This is the Latest News</p>
+                                    <p class="fs-6">Date: 12th March 2022</p>
+                                 </div>   
+                            </div>
+                        </a>
+                        <a href="#" class="col-12 mb-2 post-wrap">
+                            <div class="post-wrap">
+                                <img src="assets/img/posts/news-2.jpg" alt="" class="img-fluid side-post">
+                                 <div class="overlay"></div>
+                                 <div class="caption">
+                                    <p class="fs-4 fw-bold title">This is the Latest News</p>
+                                    <p class="fs-6">Date: 12th March 2022</p>
+                                 </div>   
+                            </div>  
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Recent Posts -->
+
+    <section id="recent">
+        <div class="container">
+            <p class="fs-3 fw-bold">Recent Posts</p>
+
+            <div class="owl-carousel">
+                <div class="item">
+                    <div class="post-wrap">
+                        <img src="assets/img/posts/news-1.jpg" alt="" class="img-fluid">
+                        <div class="overlay"></div>
+                        <div class="caption">
+                            <p class="fs-4 fw-bold">This is 1</p>
+                            <p class="fs-6">
+                                Date: 22nd March 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="post-wrap">
+                        <img src="assets/img/posts/news-1.jpg" alt="" class="img-fluid">
+                        <div class="overlay"></div>
+                        <div class="caption">
+                            <p class="fs-4 fw-bold">This is 2</p>
+                            <p class="fs-6">
+                                Date: 22nd March 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="post-wrap">
+                        <img src="assets/img/posts/news-1.jpg" alt="" class="img-fluid">
+                        <div class="overlay"></div>
+                        <div class="caption">
+                            <p class="fs-4 fw-bold">This is 3</p>
+                            <p class="fs-6">
+                                Date: 22nd March 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="post-wrap">
+                        <img src="assets/img/posts/news-1.jpg" alt="" class="img-fluid">
+                        <div class="overlay"></div>
+                        <div class="caption">
+                            <p class="fs-4 fw-bold">This is 4</p>
+                            <p class="fs-6">
+                                Date: 22nd March 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="post-wrap">
+                        <img src="assets/img/posts/news-1.jpg" alt="" class="img-fluid">
+                        <div class="overlay"></div>
+                        <div class="caption">
+                            <p class="fs-4 fw-bold">This is 5</p>
+                            <p class="fs-6">
+                                Date: 22nd March 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+               
+              </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- Footer -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/lib/owl-carousel/js/owl.carousel.min.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            center: true,
+            items:2,
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:3
+                }
+            }
+        });
+    </script>
+</body>
+</html>
